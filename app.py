06fd30910f5afd68a02f5400c9b314daf7ebab4a
Arthur1802 <arthur.barreto@soulasalle.com.br>
2023-07-26 14:33:52 -0300

    Project rebooted

--- app.py
+++ app.py
@@ -1,45 +1,15 @@
-import os
-import datetime
+import sqlite3
 
-from cs50 import SQL
-from flask import Flask, app, render_template, request, redirect, url_for, flash, session, jsonify
-from flask_session import Session
-from sqlalchemy import desc
-from werkzeug.security import generate_password_hash, check_password_hash
-from helpers import apology, login_required, lookup, usd
+conn = sqlite3.connect('tasktracker.db')
 
-app = Flask(__name__)
+db = conn.cursor()
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+db.execute('''
+           CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, hased_password TEXT, dateBirth DATE)
+           ''')
 
-# db = SQL("sqlite:///list.db")
+db.execute('''
+           CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, title TEXT, description TEXT, dateCreated DATE, dateStart DATE, dateEnd DATE, status DATE)
+           ''')
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if not username:
-            return apology('Must provide username', 403)
-        
-        if not password:
-            return apology('Must provide password', 403)
-        
-        # check = db.execute('''
-        #                    SELECT *
-        #                    FROM users
-        #                    WHERE username = ? AND
-        #                    password = ?
-        #                    ''', username, password)
-    
-    else:
-        return render_template('login.html')
-        
-
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    return apology('TODO')
+conn.close()
