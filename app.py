fad12a6f943066a487506015eca048090abf9809
Arthur1802 <arthur.barreto@soulasalle.com.br>
2023-07-26 17:14:12 -0300

    app.py file modified

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def register():
             return apology('email already exists', 403)
         
         new_session = db.execute('''
-                                 INSERT INTO users (name, email, hashed_password, dateBirth DATE)
+                                 INSERT INTO users (name, email, hashed_password, dateBirth)
+                                 VALUES (?, ?, ?, ?)
                                  ''', name, email, generate_password_hash(password), fBirthDate)
         
         session['user_id'] = new_session
@@ -92,16 +93,20 @@ def login():
 
     if request.method == 'POST':
 
-        if not request.form.get('username'):
-            return apology('must provide username', 403)
+        if not request.form.get('email'):
+            return apology('must provide email', 403)
 
         elif not request.form.get('password'):
             return apology('must provide password', 403)
 
-        rows = db.execute('SELECT * FROM users WHERE username = ?', request.form.get('username'))
+        rows = db.execute('''
+                          SELECT * 
+                          FROM users 
+                          WHERE email = ?
+                          ''', request.form.get('email'))
 
-        if len(rows) != 1 or not check_password_hash(rows[0]['hash'], request.form.get('password')):
-            return apology('invalid username and/or password', 403)
+        if len(rows) != 1 or not check_password_hash(rows[0]['hashed_password'], request.form.get('password')):
+            return apology('invalid email and/or password', 403)
 
         session['user_id'] = rows[0]['id']
 
