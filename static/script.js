36d5037ce75291fafa3e3dc71d84e48fdde95fc3
Arthur1802 <arthur.barreto@soulasalle.com.br>
2023-08-01 17:57:05 -0300

    Profile page style updated

--- static/script.js
+++ static/script.js
@@ -30,3 +30,26 @@ function restoreOriginalData(event) {
         inputField.value = inputField.dataset.originalValue;
     }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    // Get references to the eye icons
+    const seePasswdIcon = document.getElementById('see_passwd');
+    const notSeePasswdIcon = document.getElementById('not_see_passwd');
+
+    seePasswdIcon.addEventListener('click', function () {
+        const passwdInput = document.getElementById('confPasswd');
+        const inputType = passwdInput.getAttribute('type');
+        passwdInput.setAttribute('type', inputType === 'text' ? 'password' : 'text');
+
+        seePasswdIcon.classList.toggle('fa-eye');
+        seePasswdIcon.classList.toggle('fa-eye-slash');
+    });
+
+    notSeePasswdIcon.addEventListener('click', function () {
+        const passwdInput = document.getElementById('confPasswd');
+        const inputType = passwdInput.getAttribute('type');
+        passwdInput.setAttribute('type', inputType === 'text' ? 'password' : 'text');
+
+        notSeePasswdIcon.classList.toggle('fa-eye');
+        notSeePasswdIcon.classList.toggle('fa-eye-slash');
+    });
+});
